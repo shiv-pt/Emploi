7461ea9cfee09a071794d37a36ea2f2e454382ab
Shivam Prajapati <63106269+shiv-pt@users.noreply.github.com>
2022-09-07 20:43:12 +0530

    Initial

---
+++ comps/navbar.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+    return ( 
+        <nav>
+        <div className="logo">
+            <Image src="/logo.png" height={77} width={128}/>
+            </div>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/ninjas"><a>Ninja Listing</a></Link>
+        
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+      <Head>
+        <title>
+          Ninja List | About 
+        </title>
+        <meta name="keywords" content="ninjas"/>
+      </Head> 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem sapiente minima possimus, iste quidem quasi saepe temporibus eaque, velit aspernatur inventore magni, ratione aut odio molestiae facere error consectetur ducimus?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem sapiente minima possimus, iste quidem quasi saepe temporibus eaque, velit aspernatur inventore magni, ratione aut odio molestiae facere error consectetur ducimus?</p>
+        </div>
+        </>
+    );
+}
+ 
+export default About;
